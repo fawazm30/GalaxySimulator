43bee473ac46d23a5b4e5b7da63c6f3a36fee964
fmansoor-03 <fawazarshad30@gmail.com>
2026-05-27 19:27:58 -0600

    added a singular white particle >:)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 #include <glad/glad.h>
 #include <iostream>
 
+const char* vertexShaderSource = "#version 410 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos, 1.0); gl_PointSize = 10.0;\n" // Set the position of the vertex
+    "}\n";
+
+const char* fragmentShaderSource = "#version 410 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n" // Set the color of the fragment (white)
+    "}\n";
+
 // Error callback function for GLFW
 static void error_callback(int error, const char* description)
 {
@@ -26,6 +40,10 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on MacOS
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    float vertex[] = {
+        0.0f, 0.0f, 0.0f
+    };
+
     GLFWwindow* window = glfwCreateWindow(640, 480, "Galaxy Simulator", NULL, NULL);
     if (!window)
     {
@@ -38,16 +56,57 @@ int main(void) {
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress); // Load OpenGL function pointers using GLAD and GLFW's function loader
+    glEnable(GL_PROGRAM_POINT_SIZE); // Enable setting point size in the vertex shader
     glfwSwapInterval(1);
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Vertex Shader source code
+    const GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Fragment Shader source code
+    const GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link shaders to create a shader program
+    const GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
  
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.02f, 0.02f, 0.05f, 1.0f); // dark blue-black
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS, 0, 1);  
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
  
     glfwDestroyWindow(window);
  
